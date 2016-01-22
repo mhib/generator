62fa18ae2282dd5c78d7d9b1e858c237546708dd
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-01-22 15:20:56 +0100

    Minor bugfix

--- post.c
+++ post.c
@@ -8,7 +8,7 @@ typedef struct Conf {
     struct tm date;
 } Conf;
 
-int post_diff(Post * l, Post * p) {
+double post_diff(Post * l, Post * p) {
     return difftime(mktime(&(l -> published_at)), mktime(&(p -> published_at)));
 }
 
@@ -296,7 +296,7 @@ void new_post_file(char *directory, sds name) {
     fputs("title: ", f) ;
     fputs(name, f);
     fputs("\n", f);
-    strftime(t, 256, "date: %Y-%m-%dT%h:%M:%S\n", timeinfo);
+    strftime(t, 256, "date: %Y-%m-%dT%H:%M:%S\n", timeinfo);
     fputs(t, f);
     fputs("---\n", f);
     fclose(f);
